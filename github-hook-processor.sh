72271b640cf221399e175c5e41ec64b0203c0049
Matthias Steinbauer <matthias@steinbauer.org>
2017-04-11 21:04:01 +0200

    Bash script actually executing the hooks

---
+++ github-hook-processor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+die() {
+	echo >&2 "$@"
+	exit 1
+}
+
+[ "$#" -eq 1 ] || die "usage: ${0} <reponame>"
+
+REPO_NAME="${1}"
+SCRIPT_PATH="/usr/lib/github-hook-processor/${REPO_NAME}.sh"
+
+if [[ -x "${SCRIPT_PATH}" ]] ; then
+	if [[ -x "/usr/bin/sudo" ]] ; then
+		/usr/bin/sudo "${SCRIPT_PATH}"
+	else
+		"${SCRIPT_PATH}"
+	fi
+else
+	die "Cannot find script ${SCRIPT_PATH}"
+fi
