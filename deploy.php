fa83e8fabe1ef51a14093e29b5ef6be29c265c9d
Matthias Steinbauer <matthias@steinbauer.org>
2017-04-11 20:56:26 +0200

    Parse repo and execute shell script

--- deploy.php
+++ deploy.php
@@ -1,12 +1,12 @@
 <?php
+header('Content-type: application/json');
 
 $entityBody = file_get_contents('php://input');
 
-$myfile = fopen("/tmp/github.json", "w");
-fwrite($myfile, $entityBody);
-fclose($myfile);
-
 $githubHook = json_decode($entityBody);
 if(isset($githubHook)) {
-	// perform the actual processing
+	$repoName = $githubHook['repository']['name'];
+	shell_exec('/usr/local/bin/github-hook-processor.sh ' . $repoName);
 }
+
+echo '{"message": "This is to notify you that I did what you told me"}';
