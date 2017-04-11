85b323cffc11683e5d13e13ccb0afb0df3edffda
Matthias Steinbauer <matthias@steinbauer.org>
2017-04-11 20:18:44 +0200

    Write json to /tmp

--- deploy.php
+++ deploy.php
@@ -1,6 +1,11 @@
 <?php
 
 $entityBody = file_get_contents('php://input');
+
+$myfile = fopen("/tmp/github.json", "w");
+fwrite($myfile, $entityBody);
+fclose($myfile);
+
 $githubHook = json_decode($entityBody);
 if(isset($githubHook)) {
 	// perform the actual processing
